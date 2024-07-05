4f6141550f2450e79c8cdf17c3160bd2cb659a65
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2024-07-05 15:12:51 +0530

    Extend core classes

--- src/core/data-response/data-response.ts
+++ src/core/data-response/data-response.ts
@@ -1,8 +1,15 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { Metadata } from '../metadata/metadata';
 
 export class DataResponse<T> {
-  constructor(
-    private data: T,
-    private metadata?: Metadata<T>,
-  ) {}
+  @ApiProperty()
+  public data: T;
+
+  @ApiProperty({ required: false })
+  public metadata;
+
+  constructor(data: T) {
+    this.data = data;
+    this.metadata = new Metadata(data);
+  }
 }
--- src/core/metadata/metadata.ts
+++ src/core/metadata/metadata.ts
@@ -1,4 +1,7 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class Metadata<T> {
+  @ApiProperty({ required: false })
   count?: number;
 
   constructor(data: T) {
