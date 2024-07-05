bf7e95cad83d699356e6ccd3c7b0891a040b9b09
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2024-07-05 10:47:58 +0530

    Integrate Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import * as metadata from 'package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle(metadata.name)
+    .setVersion(metadata.version)
+    .setDescription(metadata.description)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
