cb27f09249e21b1add18e94a81edc43355efe4d1
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2024-07-08 07:47:34 +0530

    Create RequestLoggerMiddleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { AuthModule } from '@controllers/auth/auth.module';
 import { HealthModule } from '@controllers/health/health.module';
 import { UsersModule } from '@controllers/users/users.module';
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RequestLoggerMiddleware } from './middleware/request-logger/request-logger.middleware';
 import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
@@ -11,4 +12,9 @@ import { PrismaModule } from './modules/prisma/prisma.module';
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  // Setup middleware functions here
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestLoggerMiddleware).forRoutes('*');
+  }
+}
---
+++ src/middleware/request-logger/request-logger.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, Logger, NestMiddleware } from '@nestjs/common';
+import { NextFunction, Request, Response } from 'express';
+
+@Injectable()
+export class RequestLoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger(RequestLoggerMiddleware.name);
+
+  use(request: Request, response: Response, next: NextFunction): void {
+    const { method, originalUrl } = request;
+    const start = Date.now();
+
+    response.on('finish', () => {
+      const { statusCode } = response;
+      const duration = Date.now() - start;
+      this.logger.log(`${method} ${originalUrl} ${statusCode} - ${duration}ms`);
+    });
+
+    next();
+  }
+}
