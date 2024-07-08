40e2a55c34f8a2cec82da3dcc9842fe3f492845f
Pivithuru Jayakody <pivithurujayakody@gmail.com>
2024-07-08 08:49:27 +0530

    Include helmet package

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import helmet from 'helmet';
 import * as metadata from 'package.json';
 import { AppModule } from './app.module';
 
@@ -13,6 +14,8 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
+
+  app.use(helmet());
 
   await app.listen(3000);
 }
